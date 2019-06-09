a07741bf31d36a658f0405c8d80cda728b0fc790
ashimgit <ashimnb87@gmail.com>
2019-06-09 17:39:11 +0530

    bug fixes to be a client of eureka

--- src/main/java/com/add/microaddservice/MicroAddServiceApplication.java
+++ src/main/java/com/add/microaddservice/MicroAddServiceApplication.java
@@ -2,10 +2,12 @@ package com.add.microaddservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableEurekaClient
 @SpringBootApplication
-@ComponentScan("com.add.controller")
+@ComponentScan("com.add.controller.**")
 public class MicroAddServiceApplication {
 
 	public static void main(String[] args) {
